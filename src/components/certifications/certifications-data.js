9399aba05c19194c1ecf564bdb5890d1cf983f20
ljparfan <92625708+ljparfan@users.noreply.github.com>
2022-07-08 00:18:25 +0800

    add mta on certifications

--- src/components/certifications/certifications-data.js
+++ src/components/certifications/certifications-data.js
@@ -1,5 +1,32 @@
 const CERTIFICATIONS_DATA = [
   {
+    id: 5,
+    title: "MTA: Software Development Fundamentals - Certified 2017",
+    issuerImageFileName: "mtasoftdev.png",
+    issuer: "Microsoft",
+    issueDate: new Date(2017, 4),
+    certificateUrl:
+      "https://www.credly.com/badges/98bde737-ff9a-4f26-9288-3c470c6485a6/public_url",
+  },
+  {
+    id: 6,
+    title: "MTA: Networking Fundamentals - Certified 2017",
+    issuerImageFileName: "mtanetwork.png",
+    issuer: "Microsoft",
+    issueDate: new Date(2017, 4),
+    certificateUrl:
+      "https://www.credly.com/badges/76486449-8955-4e6f-83b9-2f7f9ec5c478/public_url",
+  },
+  {
+    id: 7,
+    title: "MTA: Database Fundamentals - Certified",
+    issuerImageFileName: "mtadb.png",
+    issuer: "Microsoft",
+    issueDate: new Date(2017, 4),
+    certificateUrl:
+      "https://www.credly.com/badges/3fc4fb5b-d661-45d6-bd80-de3795ea06ec/public_url",
+  },
+  {
     id: 8,
     title: "Ultimate Java Part 3: Advanced Topics",
     issuerImageFileName: "a7bec93b7cd9c89e170f656d61ad5a80d32e8128.png",
